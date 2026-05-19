eec1f031b8145cd56c1c19348ff339a8a7f35f94
Roych <156516367+Roychgod@users.noreply.github.com>
2026-05-19 14:03:33 +0200

    Add module info files for ProcessWire directory

---
+++ NativeAnalytics.info.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+
+$info = array(
+    'title' => 'NativeAnalytics',
+    'summary' => 'Native first-party analytics dashboard for ProcessWire with traffic, compare, exports and event tracking.',
+    'version' => 1020,
+    'author' => 'Pyxios - Roych (www.pyxios.com)',
+    'href' => 'https://processwire.com/talk/topic/31808-native-analytics-%E2%80%94-a-native-analytics-module-for-processwire/',
+    'repo' => 'https://github.com/Roychgod/NativeAnalytics',
+    'icon' => 'line-chart',
+    'autoload' => true,
+    'singular' => true,
+    'requires' => array('ProcessWire>=3.0.173', 'PHP>=7.4', 'LazyCron'),
+);
---
+++ ProcessNativeAnalytics.info.php
@@ -0,0 +1,15 @@
+<?php namespace ProcessWire;
+
+$info = array(
+    'title' => 'NativeAnalytics Dashboard',
+    'summary' => 'Dashboard for the NativeAnalytics module.',
+    'version' => 1020,
+    'author' => 'Pyxios - Roych (www.pyxios.com)',
+    'permission' => 'nativeanalytics-view',
+    'icon' => 'area-chart',
+    'requires' => array('NativeAnalytics'),
+    'page' => array(
+        'name' => 'native-analytics',
+        'title' => 'Analytics',
+    ),
+);
